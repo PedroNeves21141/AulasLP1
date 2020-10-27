ad9bf53466d1e7ba9710eae8da586b450673f8a3
PedroNeves <a21141@alunos.ipca.pt>
2020-10-27 22:42:44 +0000

    Inicial

---
+++ aula.c
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @file aula.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief Programa que não sei o quê.... 
+ * 
+ * @return int 
+ */
+int main() 
+{
+    int x = 1;
+    printf("%d\n", x);
+}
+
+/**
+ * @brief 
+ * 
+ * @param x
+ * @param y
+ * @return int
+ */
+
+int soma(int x, int y) {
+    return x + y;
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+#makefile
+#by
+#date:
+all: aula.c 
+	gcc -o aula.exe aula.c
+	
+clean:
+	rm -f *.o *.exe
